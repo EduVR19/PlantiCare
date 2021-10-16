31cff66b25b344af9b5e7e4d8ad286c7d0782570
Eduardo Reyna <49138106+EduVR19@users.noreply.github.com>
2021-10-16 17:05:20 -0500

    App con front-end y back-end Falta conexión a Firebase y envío de notificaciones

--- app/src/main/java/com/example/plantcare/MainActivity.kt
+++ app/src/main/java/com/example/plantcare/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.plantcare
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        var CurrentProgressHumedity = 70
+        var CurrentProgressWater= 60
+        var progressBar: ProgressBar? = null
+        var progressBar2: ProgressBar? = null
+
+
+
+        progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        progressBar2 = findViewById<ProgressBar>(R.id.progressBar2)
+
+        progressBar.setProgress(CurrentProgressHumedity)
+        progressBar2.setProgress(CurrentProgressWater)
+
+        progressBar.setMax(100)
+        progressBar2.setMax(100)
+
+
     }
 }
