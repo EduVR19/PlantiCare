c361a67d9a2f357f931b5d6431ec89b9258c6121
Sergio <sergio27alberto@gmail.com>
2021-10-16 21:43:08 -0500

    Actualizar valores en Interfaz (Text View y ProgressBar)

--- app/src/main/java/com/example/plantcare/MainActivity.kt
+++ app/src/main/java/com/example/plantcare/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.example.plantcare
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,23 +20,47 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-        var CurrentProgressHumedity = 70
-        var CurrentProgressWater= 60
         var progressBar: ProgressBar? = null
         var progressBar2: ProgressBar? = null
-
-
-
+        var textohumedad= findViewById<TextView>(R.id.textView3)
+        var textoagua= findViewById<TextView>(R.id.textView4)
         progressBar = findViewById<ProgressBar>(R.id.progressBar)
         progressBar2 = findViewById<ProgressBar>(R.id.progressBar2)
 
-        progressBar.setProgress(CurrentProgressHumedity)
-        progressBar2.setProgress(CurrentProgressWater)
+        //Para entrar a Firebase
+        val Database= Firebase.database
+        val myref= Database.getReference("sensores")
 
-        progressBar.setMax(100)
-        progressBar2.setMax(100)
+
+        //Intentar si existe un cambio
+        val userListener= object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val user = dataSnapshot.getValue<User>()
+                //Evaluar los datos desde BD
+                var currentProgressHumedity = user?.humedad
+                var currentProgressWater= user?.agua
+                //Mandar a Interfaz
+                if (currentProgressHumedity != null) {
+                    progressBar.setProgress(currentProgressHumedity)
+                    var texto=currentProgressHumedity.toString()+"%"
+                    textohumedad.setText(texto)
+                }
+                if (currentProgressWater != null) {
+                    progressBar2.setProgress(currentProgressWater)
+                    var texto2= currentProgressWater.toString()+"%"
+                    textoagua.setText(texto2)
+
+                }
+                progressBar.setMax(100)
+                progressBar2.setMax(100)
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "userData:onCancelled", databaseError.toException())
+            }
+        }
+        myref.addValueEventListener(userListener)
 
 
     }
---
+++ app/src/main/java/com/example/plantcare/User.kt
@@ -0,0 +1,3 @@
+package com.example.plantcare
+
+data class User(var agua: Int?=0, var humedad: Int?=0)
