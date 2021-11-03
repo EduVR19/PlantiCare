3e2bb9544c43bedb44136f6c0d06a7ccde0be11f
Eduardo Reyna <49138106+EduVR19@users.noreply.github.com>
2021-11-03 08:13:18 -0600

    Se agregó el sketch de la conexión del ESP32 a Firebase

---
+++ Sketch/ESP32-Firebase/ESP32-Firebase.ino
@@ -0,0 +1,98 @@
+#include <Arduino.h>
+#if defined(ESP32)
+  #include <WiFi.h>
+#elif defined(ESP8266)
+  #include <ESP8266WiFi.h>
+#endif
+#include <Firebase_ESP_Client.h>
+
+//Provide the token generation process info.
+#include "addons/TokenHelper.h"
+//Provide the RTDB payload printing info and other helper functions.
+#include "addons/RTDBHelper.h"
+
+// Insert your network credentials
+#define WIFI_SSID "HOME-85F4"
+#define WIFI_PASSWORD "RD9TWADEWH0ENH8S"
+
+// Insert Firebase project API Key
+#define API_KEY "AIzaSyCHWw_p7AncLedmtzjT3v5R3m_v2dzbhdQ"
+
+// Insert RTDB URLefine the RTDB URL */
+#define DATABASE_URL "https://plantcare-61ea9-default-rtdb.firebaseio.com/" 
+
+//Define Firebase Data object
+FirebaseData fbdo;
+
+FirebaseAuth auth;
+FirebaseConfig config;
+
+unsigned long sendDataPrevMillis = 0;
+bool signupOK = false;
+
+void setup(){
+  Serial.begin(115200);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  /* Assign the api key (required) */
+  config.api_key = API_KEY;
+
+  /* Assign the RTDB URL (required) */
+  config.database_url = DATABASE_URL;
+
+  /* Sign up */
+  if (Firebase.signUp( &config, &auth, "", "" ) )
+  {
+    Serial.println("ok");
+    signupOK = true;
+  }
+  else
+  {
+    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+  }
+
+  /* Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+  
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+}
+
+void loop(){
+  if ( Firebase.ready() && signupOK && ( millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0 ) ){
+    
+    sendDataPrevMillis = millis();
+    
+    // Nivel de Humedad con la ruta: test/int
+    if (Firebase.RTDB.setInt(&fbdo, "sensores/humedad", random(0,100) ) ){
+      Serial.println("PASSED");
+      Serial.println("PATH: " + fbdo.dataPath());
+      Serial.println("TYPE: " + fbdo.dataType());
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+
+    
+    // Nivel de agua
+    if (Firebase.RTDB.setFloat(&fbdo, "sensores/agua", random(0,100) ) ){
+      Serial.println("PASSED");
+      Serial.println("PATH: " + fbdo.dataPath());
+      Serial.println("TYPE: " + fbdo.dataType());
+    }
+    else {
+      Serial.println("FAILED");
+      Serial.println("REASON: " + fbdo.errorReason());
+    }
+  }
+}
